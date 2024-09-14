dbdebbbf384b282113a9c27b783a0e5ef388fd99
Battlesquid <esanoluwamayowa@gmail.com>
2024-09-14 12:33:27 -0700

    chore: fix tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,32 +1,31 @@
-import { equal, notEqual } from "assert";
-import CurlImpersonate from "../src";
+import { equal } from "assert";
+import { RequestBuilder } from "../src";
 
 test("Returns a successful GET reponse on TLS Fingerprinting protected URL", async () => {
-    let ci = new CurlImpersonate("", {
-        method: "GET",
-        headers: {
-            "x-user-key": "amiami_dev"
-        },
-        impersonate: "firefox-109",
-        verbose: true,
-    });
-    let req = await ci.makeRequest("https://api.amiami.com/api/v1.0/items?s_keywords=touhou%20plush&pagecnt=2&pagemax=30&lang=eng");
-    console.log(req)
-    expect(equal(req.status, 200))
-})
+    const response = await new RequestBuilder()
+        .url("https://api.amiami.com/api/v1.0/items?s_keywords=touhou%20plush&pagecnt=2&pagemax=30&lang=eng")
+        .header("x-user-key", "amiami_dev")
+        .preset({ name: "firefox", version: "109" })
+        .send();
+
+    expect(response.stderr).toBeUndefined();
+    expect(equal(response.details?.http_code, 200));
+});
 
 test("Returns a successful POST reponse", async () => {
-    let ci = new CurlImpersonate("https://httpbin.org/post", {
-        method: "POST",
-        headers: {
-            "user-agent": "node-curl-impersonate Test Suite -- contact@wearr.dev"
-        },
-        body: {
-            title: "foo",
-            body: "bar",
-            userId: 1,
-        }
-    });
-    let req = await ci.makeRequest()
-    expect(equal(req.status, 200))
+    const body = {
+        title: "foo",
+        body: "bar",
+        userId: 1,
+    };
+
+    const response = await new RequestBuilder()
+        .url("https://httpbin.org/post")
+        .method("POST")
+        .header("user-agent", "node-curl-impersonate Test Suit")
+        .flag("-d", JSON.stringify(body))
+        .send();
+
+    expect(response.stderr).toBeUndefined();
+    expect(equal(response.details?.http_code, 200));
 })
